05cb149f83d1e31afe92dc7276b439e9b16303b0
Paul Tiller <pftiller@gmail.com>
2018-06-10 16:02:27 -0500

    setting up local server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 1234;
+
+//Static page
+app.use(express.static('/public'))
+
+//Body Parser middleware
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(port,()=>{
+    console.log('Server running on Port', port)
+});
