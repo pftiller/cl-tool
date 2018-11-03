344b94c7e8850ff75d060d210404b15461b10ff6
Paul Tiller <pftiller@gmail.com>
2018-11-03 16:58:10 -0500

    adjustments to randomization

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,14 @@
 console.log('client.js is loaded');
 let letter = '';
 let sentences = [];
+let position;
+let company;
+let h2 = document.querySelector('h2');
+let salutation = document.querySelector('.salutation');
+let first_paragraph = document.querySelector('.first_paragraph');
+let second_paragraph = document.querySelector('.second_paragraph');
+let third_paragraph = document.querySelector('.third_paragraph');
+let fourth_paragraph = document.querySelector('.fourth_paragraph');
 $(document).ready(onReady); 
 
 function onReady() {
@@ -9,8 +17,8 @@ function onReady() {
     //event listeners
     $('.inputs').on('submit', function(event) {
         event.preventDefault();
-        let position = $('#position').val();
-        let company = $('#company').val();
+        position = $('#position').val();
+        company = $('#company').val();
         $('input[type="text"').val('');
         console.log(`position is ${position} and company is ${company}`);    
         retrieveText()
@@ -18,20 +26,34 @@ function onReady() {
 };
 
 function retrieveText() {
-    $.get('/router').then((response) => {
+    let data = {
+        position: position,
+        company: company,
+        oneAndTwo: Math.floor(Math.random() * 2) + 1,
+        oneAndSix: Math.floor(Math.random() * 6) + 1,
+        oneAndTwelve: Math.floor(Math.random() * 12) + 1
+    }
+    $.get('/router', data).then((response) => {
         console.log('response: ', response);
         sentences = response;
-        assembleText(sentences);
+        appendToDOM(sentences);
     })
     .catch((err) => {
         console.log('error:', err);
     })
 }
-function assembleText(texts) {
-    console.log('now we are in assembleText', texts);
-for(text of texts) {
-    letter = letter + text.entry + '';
-    console.log(letter);
- }
- $('.text').append(`<p>${letter}</p>`);
+function appendToDOM(text) {
+        document.querySelector('h3').innerText = `Re: ${position} at ${company}`;
+        document.querySelector('.salutation').innerText = text[0].entry + ',';
+        document.querySelector('.first_paragraph').innerText = text[1].entry + ' ' + text[2].entry + ' ' + text[3].entry;
+        document.querySelector('.second_paragraph').innerText = text[4].entry + ' ' + text[5].entry + ' ' + text[6].entry + ' ' + text[7].entry;
+        document.querySelector('.third_paragraph').innerText = text[8].entry + ' ' + text[9].entry + ' ' +  text[10].entry;
+        document.querySelector('.fourth_paragraph').innerText = text[11].entry + ' ' + text[12].entry + ' ' + text[13].entry;
+        document.querySelector('.fifth_paragraph').innerText =  text[14].entry + ' ' + text[15].entry + ' ' + text[16].entry + ' ' + text[17].entry;
+        document.querySelector('.sixth_paragraph').innerText =   text[18].entry + ' ' + text[19].entry + ' ' + text[20].entry;
+        reset();
+}
+
+function reset() {
+   sentences = [];
 }
--- server/routes/router.js
+++ server/routes/router.js
@@ -4,47 +4,109 @@ const pool = require('../modules/pool')
 
 
 router.get('/', (req, res) => {
+    let position = req.query.position;
+    let company = req.query.company;
     const queryText =  `(SELECT *
                         FROM entries
-                        WHERE id = 1
-                        ORDER BY decimal ASC
-                        LIMIT 1)
+                        WHERE id = 1 AND subsection = 1 AND index_spot = ${Math.floor(Math.random() * 2) + 1})
                         UNION
                         (SELECT *
-                        FROM entries
-                        WHERE id = 2
-                        ORDER BY decimal ASC
-                        LIMIT 4)
+                            FROM entries
+                            WHERE id = 2 AND subsection = 1 AND index_spot = ${Math.floor(Math.random() * 2) + 1})
+                        UNION    
+                        (SELECT *
+                            FROM entries
+                            WHERE id = 2 AND subsection = 2 AND index_spot = ${Math.floor(Math.random() * 6) + 1})
+                        UNION    
+                        (SELECT *
+                            FROM entries
+                            WHERE id = 2 AND subsection = 3 AND index_spot = ${Math.floor(Math.random() * 4) + 1})
                         UNION
                         (SELECT *
-                        FROM entries
-                        WHERE id = 3
-                        ORDER BY decimal ASC
-                        LIMIT 4)
-                        UNION
+                            FROM entries
+                            WHERE id = 2 AND subsection = 4)
+                        UNION    
                         (SELECT *
-                        FROM entries
-                        WHERE id = 4
-                        ORDER BY decimal ASC
-                        LIMIT 4)
-                        UNION
+                            FROM entries
+                            WHERE id = 3 AND subsection = 1 AND index_spot = ${Math.floor(Math.random() * 2) + 1})
+                        UNION    
                         (SELECT *
-                        FROM entries
-                        WHERE id = 5
-                        ORDER BY decimal ASC
-                        LIMIT 3)
-                        UNION
+                            FROM entries
+                            WHERE id = 3 AND subsection = 2)
+                        UNION    
                         (SELECT *
-                        FROM entries
-                        WHERE id = 6
-                        ORDER BY decimal ASC
-                        LIMIT 3)
-                        ORDER BY id ASC, decimal ASC;`
-                      
+                            FROM entries
+                            WHERE id = 3 AND subsection = 3)
+                        UNION    
+                        (SELECT *
+                            FROM entries
+                            WHERE id = 3 AND subsection = 4)
+                        UNION    
+                        (SELECT *
+                            FROM entries
+                            WHERE id = 3 AND subsection = 5)
+                        UNION    
+                        (SELECT *
+                            FROM entries
+                            WHERE id = 4 AND subsection = 1)
+                        UNION
+                            (SELECT *
+                            FROM entries
+                            WHERE id = 4 AND subsection = 2 AND index_spot = ${Math.floor(Math.random() * 2) + 1})
+                        UNION
+                            (SELECT *
+                            FROM entries
+                            WHERE id = 4 AND subsection = 3 AND index_spot = ${Math.floor(Math.random() * 2) + 1})
+                        UNION
+                            (SELECT *
+                            FROM entries
+                            WHERE id = 4 AND subsection = 4 AND index_spot = ${Math.floor(Math.random() * 2) + 1})
+                        UNION
+                            (SELECT *
+                            FROM entries
+                            WHERE id = 5 AND subsection = 1 AND index_spot = ${Math.floor(Math.random() * 2) + 1})
+                        UNION
+                            (SELECT *
+                            FROM entries
+                            WHERE id = 5 AND subsection = 2 AND index_spot = ${Math.floor(Math.random() * 12) + 1})
+                        UNION
+                            (SELECT *
+                            FROM entries
+                            WHERE id = 5 AND subsection = 2 AND index_spot = ${Math.floor(Math.random() * 12) + 1})
+                        UNION
+                            (SELECT *
+                            FROM entries
+                            WHERE id = 5 AND subsection = 2 AND index_spot = ${Math.floor(Math.random() * 12) + 1})
+                        UNION
+                            (SELECT *
+                            FROM entries
+                            WHERE id = 6 AND subsection = 1)
+                        UNION
+                            (SELECT *
+                            FROM entries
+                            WHERE id = 6 AND subsection = 2)
+                        UNION
+                            (SELECT *
+                            FROM entries
+                            WHERE id = 6 AND subsection = 3 AND index_spot = ${Math.floor(Math.random() * 2) + 1})
+                        UNION
+                            (SELECT *
+                            FROM entries
+                            WHERE id = 6 AND subsection = 4)    
+                        ORDER BY id ASC, subsection ASC;`
     pool.query(queryText)
         .then((result) => {
             console.log('results: ', result.rows);
-            res.send(result.rows);
+            let results = result.rows;
+            for(result of results) {
+                if(result.entry.includes('${position}') || result.entry.includes('${company}')){
+                    result.entry = result.entry.replace('${position}', `${position}`)
+                    result.entry = result.entry.replace('${company}', `${company}`);
+                    console.log(result.entry);
+                }
+            }
+           
+            res.send(results);
         })
         .catch((err) => {
             console.log('error:', err);
